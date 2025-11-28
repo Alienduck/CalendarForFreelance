601280d27595c8a250b917235d264782afbb6b87
Owen Neuillyr <jacquonneries@gmail.com>
2025-11-28 14:19:34 +0100

    edit password to password_hash

--- backend/models/auth_account.ts
+++ backend/models/auth_account.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const ZAuthAccount = z.object({
 	user_id: z.coerce.number().int().min(1),
 	email: z.email(),
-	password: z.coerce.string,
+	password_hash: z.coerce.string,
 });
 
 export type AuthAccount = z.infer<typeof ZAuthAccount>;
