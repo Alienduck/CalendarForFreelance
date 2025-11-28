42edc288c72fa98daa6ddb15507c21749bf6c765
Owen Neuillyr <jacquonneries@gmail.com>
2025-11-28 14:40:39 +0100

    rename

---
+++ frontend/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './components/ui/Button'
+
+function App() {
+  return (
+    <>
+      <h1>
+        Hello World !
+      </h1>
+      <Button text="Click me"/>
+    </>
+  )
+}
+
+export default App
