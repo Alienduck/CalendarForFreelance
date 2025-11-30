8f26aeb144597aaacb8f85e296ed2c65b7947916
Owen Neuillyr <jacquonneries@gmail.com>
2025-11-30 20:37:48 +0100

    no date take

--- src/models/availabilities.ts
+++ src/models/availabilities.ts
@@ -1,19 +1,20 @@
 import z from "zod";
 
 export const ZAvailabilities = z.object({
-	id: z.uuid(),
-	user_id: z.uuid(),
-	day_of_week: z.coerce.number().int().min(0).max(6),
-	date: z.coerce.date(),
-	start_time: z.coerce.number().int().min(0).max(23),
-	end_time: z.coerce.number().int().min(0).max(23),
-	created_at: z.coerce.date(),
+  id: z.uuid(),
+  user_id: z.uuid(),
+  day_of_week: z.coerce.number().int().min(0).max(6),
+  date: z.coerce.date(),
+  start_time: z.coerce.string(),
+  end_time: z.coerce.string(),
+  created_at: z.coerce.date(),
 });
 
 export const ZAvailabilitiesInput = ZAvailabilities.omit({
-	id: true,
-	user_id: true,
-	created_at: true,
+  id: true,
+  user_id: true,
+  created_at: true,
+  date: true,
 });
 
 export type Availabilities = z.infer<typeof ZAvailabilities>;
