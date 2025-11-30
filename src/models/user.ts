ab06d2f5ac02156b5c978714267a02a1c8867d90
Owen Neuillyr <jacquonneries@gmail.com>
2025-11-30 12:37:58 +0100

    add default values for nullable

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,20 +1,20 @@
 import z from "zod";
 
 export const ZUser = z.object({
-	id: z.uuid(),
-	username: z.string().default("username"),
-	full_name: z.string().default("no name"),
-	bio: z.string().nullable(),
-	job_title: z.string().nullable(),
-	avatar_url: z.string().nullable(),
-	created_at: z.date(),
-	updated_at: z.date(),
+  id: z.uuid(),
+  username: z.string().default("username"),
+  full_name: z.string().default("no name"),
+  bio: z.string().default("Bio in progress..."),
+  job_title: z.string().default("No job ?"),
+  avatar_url: z.string().default("path_to_default_avatar"),
+  created_at: z.date(),
+  updated_at: z.date(),
 });
 
 export const ZUserInput = ZUser.omit({
-	created_at: true,
-	updated_at: true,
-	id: true,
+  created_at: true,
+  updated_at: true,
+  id: true,
 });
 
 export const ZUserInputPartial = ZUserInput.partial();
